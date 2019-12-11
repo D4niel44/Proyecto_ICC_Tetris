7368680b206a5a681fd82f7757f9f8ad3faeaacb
L-uis <jluisp.m.19@ciencias.unam.mx>
2019-12-10 19:58:33 -0600

    Piezas

--- src/main/java/juego/pieza/Z.java
+++ src/main/java/juego/pieza/Z.java
@@ -23,5 +23,71 @@ public class Z extends Pieza {
 
     public Z(Posicion posicion) {
         super(posicion);
+        matriz = estadoPieza[estado.obtenerEstado()];
+    }
+
+    public void rotarSentidoHorario() {
+    	Bloque[][] matrizRotada = estadoPieza[estado.siguienteEstado().obtenerEstado()];
+    	boolean puedeRotar = false;
+    	int movimientoColumnas = -1 ;
+    	int movimientoFilas = 1;
+
+        // maneja los wall kicks
+        while (movimientoColumnas <= 1 && !puedeRotar) {
+                movimientoColumnas++;
+                if (this.puedeMoverse(matrizRotada,
+                        posicion.moverPosicion(0, movimientoColumnas))) {
+                    puedeRotar = true;
+                }
+            }
+
+        // maneja los floor kicks en caso de que no se halla podido rotar previamente
+        while (movimientoFilas >= -1 && !puedeRotar) {
+            movimientoFilas--;
+            if (this.puedeMoverse(matrizRotada,
+                    posicion.moverPosicion(0, movimientoFilas))) {
+                puedeRotar = true;
+            }
+        }
+
+        // si se puede rotar la piesa la rota
+        if (puedeRotar) {
+            this.posicion = posicion.moverPosicion(movimientoFilas, movimientoColumnas);
+            this.matriz = matrizRotada;
+            estado = estado.siguienteEstado();
+        }
+
+    }
+
+    public void rotarSentidoAntihorario() {
+    	Bloque[][] matrizRotada = estadoPieza[estado.anteriorEstado().obtenerEstado()];
+    	boolean puedeRotar = false;
+    	int movimientoColumnas = -1 ;
+    	int movimientoFilas = 1;
+
+        // maneja los wall kicks
+        while (movimientoColumnas <= 1 && !puedeRotar) {
+                movimientoColumnas++;
+                if (this.puedeMoverse(matrizRotada,
+                        posicion.moverPosicion(0, movimientoColumnas))) {
+                    puedeRotar = true;
+                }
+            }
+
+        // maneja los floor kicks en caso de que no se halla podido rotar previamente
+        while (movimientoFilas >= -1 && !puedeRotar) {
+            movimientoFilas--;
+            if (this.puedeMoverse(matrizRotada,
+                    posicion.moverPosicion(0, movimientoFilas))) {
+                puedeRotar = true;
+            }
+        }
+
+        // si se puede rotar la piesa la rota
+        if (puedeRotar) {
+            this.posicion = posicion.moverPosicion(movimientoFilas, movimientoColumnas);
+            this.matriz = matrizRotada;
+            estado = estado.anteriorEstado();
+        }
     }
 }
